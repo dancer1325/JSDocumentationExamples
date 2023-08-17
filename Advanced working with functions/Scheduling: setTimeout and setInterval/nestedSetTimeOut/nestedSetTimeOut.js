928b343f407a246f77b9967c7113144ebbd16817
dancer1325 <alfredotic0809@gmail.com>
2023-08-17 23:39:55 +0200

    doc(AdvancedWorkingWithFunctions): add nestedSetTimeOut commentary

--- Advanced working with functions/Scheduling: setTimeout and setInterval/nestedSetTimeOut/nestedSetTimeOut.js
+++ Advanced working with functions/Scheduling: setTimeout and setInterval/nestedSetTimeOut/nestedSetTimeOut.js
@@ -1,5 +1,6 @@
 // 1. nested setTimeOut with callback & delayInms
 let timerId = setTimeout(
+    //  The key is to define the function, to call afterwards
     function tick() {
         alert('tick');
         timerId = setTimeout(tick, 2000);
